8556e1b843ab61053889e3f974387d65335bbfeb
James O'Kane <orbiccode@gmail.com>
2025-12-24 17:31:23 +0000

    use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+'use client';
+
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Excel to Word Summariser</h1>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input type='file' />
+        </form>
+      </div>
     </main>
   );
 }
