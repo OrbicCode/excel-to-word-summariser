bd144655fb0ebb4dc3d5c72ecf2c784b5a002fb3
James O'Kane <orbiccode@gmail.com>
2025-12-24 17:30:56 +0000

    changed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const akkuratPro = localFont({
+  src: [
+    {
+      path: './fonts/AkkuratPro/AkkuratPro.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/AkkuratPro/Akkurat-Bold.woff2',
+      weight: '700',
+      style: 'bold',
+    },
+  ],
+  variable: '--akkurat-pro',
 });
 
 export const metadata: Metadata = {
@@ -24,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+      <body className={`${akkuratPro.variable} ${akkuratPro.variable}`}>{children}</body>
     </html>
   );
 }
