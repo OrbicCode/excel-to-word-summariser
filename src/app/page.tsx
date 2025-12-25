341901bb4340cd99b6b9d6136d53d249dfcf8d83
James O'Kane <orbiccode@gmail.com>
2025-12-25 17:51:32 +0000

    heading change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,11 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Excel to Word AI Summariser</h1>
+      <h1>
+        Prototype
+        <br />
+        Excel to Word AI Summariser
+      </h1>
       <div className={styles.formContainer}>
         <UploadForm />
       </div>
