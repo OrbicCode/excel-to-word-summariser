eb95a6a3ca961ca552927aa9e88fb2c04ee7ce5e
James O'Kane <orbiccode@gmail.com>
2025-12-25 15:00:05 +0000

    created buffer object

--- src/app/api/summerise/route.ts
+++ src/app/api/summerise/route.ts
@@ -2,6 +2,14 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
-  console.log(formData);
+  const file = formData.get('file') as File;
+
+  if (!file) {
+    return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
+  }
+
+  const arrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  console.log(buffer);
   return NextResponse.json({ message: 'hello', formData: formData });
 }
