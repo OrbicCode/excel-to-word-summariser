a4c4442a48a75c91677a904d9e38e4f65e2cf35f
James O'Kane <orbiccode@gmail.com>
2025-12-25 15:23:17 +0000

    XLSX processing

--- src/app/api/summerise/route.ts
+++ src/app/api/summerise/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+import * as XLSX from 'xlsx';
 
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
@@ -10,6 +11,12 @@ export async function POST(req: NextRequest) {
 
   const arrayBuffer = await file.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
-  console.log(buffer);
+
+  const workbook = XLSX.read(buffer, { type: 'buffer' });
+  const sheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[sheetName];
+  const data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+  const dataString = JSON.stringify(data);
   return NextResponse.json({ message: 'hello', formData: formData });
 }
