d4df3aa36bbe3af63fc593f964ba186858b7aeb0
James O'Kane <orbiccode@gmail.com>
2025-12-25 18:00:42 +0000

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' href='/favicon.ico' sizes='any' />
+      </head>
       <body className={`${akkuratPro.variable} ${akkuratPro.variable}`}>{children}</body>
     </html>
   );
